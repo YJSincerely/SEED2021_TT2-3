6011f6ef0035ee9e6963544308ec015b7391ebde
James <jamesteoh0104@gmail.com>
2020-12-11 13:07:15 +0800

    Basic Navigation bar
    - Landing Page
    - Account Balance PAge (with temp fixed csutID)

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { AccountBalance } from "./components/AccountBalance";
+import { Navbar } from "./components/Navbar";
 
 function App() {
 	return (
 		<div className="App">
 			<Router>
+				<Navbar />
 				<Switch>
 					<Route exact path="/">
 						Home Page
